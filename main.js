2c404e0ef6d565b609fe0a0094d07dc381970032
unknown <you@example.com>
2023-12-30 02:28:58 -0800

    my sound function

--- main.js
+++ main.js
@@ -2,6 +2,18 @@ function  Start() {
     alert('Are you ready???')
     document.getElementById('div1').innerHTML = document.getElementById('div3').innerHTML
 }
+
 function Home(){
+    alert('Are you sure?')
     window.location.href = "index.html"
 }
+
+function music(){
+    alert('Play Music?')
+    let x = document.getElementById('btns1')
+    x = document.getElementById('btns2')
+    x.play()
+}
+
+
+
